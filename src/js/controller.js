6540abd31b6466cb30c232a713d5820bd412b067
Leeroy Chakanyuka <chakanyukaleeroy4@gmail.com>
2024-12-25 05:31:51 +0200

    fetching the recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,42 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+//Getting the recipe from the API
+async function showRecipe() {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      alert(
+        `please try a new ID, this one does not exist! Code : ${response.status}`
+      );
+      throw new Error('This ID does not exist!');
+    }
+    //we use let here because we want to make a new object completely, restructuring the formatting from the API
+    let { recipe } = data.data;
+    //get recipe, from the data object
+    // console.log(recipe);
+    recipe = {
+      id: recipe.id,
+      ingredients: recipe.ingredients,
+      cookingTime: recipe.cooking_time,
+      imageURL: recipe.image_url,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceURL: recipe.sourceURL,
+      title: recipe.title,
+    };
+    // console.log(recipe);
+    // console.log(response);
+    // console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// console.log('here');
+
+showRecipe();
