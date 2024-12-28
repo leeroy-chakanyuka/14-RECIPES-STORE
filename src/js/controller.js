140175591384e99d1597973b60d043eff2909d94
Leeroy Chakanyuka <chakanyukaleeroy4@gmail.com>
2024-12-28 22:41:02 +0200

    fixed spinner!!!

--- src/js/controller.js
+++ src/js/controller.js
@@ -33,7 +33,8 @@ async function showRecipe() {
     if (!id) return;
     renderSpinner(recipeContainer);
     const response = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      // `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
     );
     const data = await response.json();
     if (!response.ok) {
