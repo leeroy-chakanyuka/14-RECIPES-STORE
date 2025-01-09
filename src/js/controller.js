1aa3e9dbd902371211b0fb8fa5254945a5769a8d
Leeroy Chakanyuka <chakanyukaleeroy4@gmail.com>
2025-01-09 09:03:36 +0200

    implementing the MVC architechture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,8 @@
-import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as model from './model';
+import recipeView from './views/recipeView';
+import recipeView from './views/recipeView';
 // console.log(icons)
 const recipeContainer = document.querySelector('.recipe');
 
@@ -12,142 +13,26 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
-function renderSpinner(elementToAttach) {
-  const html = ` <div class="spinner">
-    <svg>
-      <use href="${icons}.svg#icon-loader"></use>
-    </svg>
-  </div> 
-  `;
-  elementToAttach.innerHTML = '';
-  elementToAttach.insertAdjacentHTML('afterbegin', html);
-}
-// https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
-let temp;
-//Getting the recipe from the API
-async function showRecipe() {
+// https://forkify-api.herokuapp.com/v2
+async function controlRecipe() {
   try {
     const id = window.location.hash.slice(1);
-    // console.log(id);
     if (!id) return;
-    renderSpinner(recipeContainer);
-    // GETTING THE RECIPE FROM THE API
+    recipeView.renderSpinner();
+    // 1. GETTING THE RECIPE FROM THE API
     await model.loadRecipe(id);
-    const { recipe } = model.state;
-    //RENDERING THE RECIPE
-    const markupHTML = `<figure class="recipe__fig">
-          <img src="${recipe.imageURL}" alt="${
-      recipe.title
-    }" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
-
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}.svg#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-              recipe.cookingTime
-            }</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}.svg#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-              recipe.servings
-            }</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}.svg#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}.svg#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}.svg#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${icons}.svg#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipe.ingredients
-            .map(ing => {
-              return `
-              <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}.svg#icon-check"></use>
-              </svg>
-              <div class=" recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-          ${ing.description}
-              </div>
-            </li>
-            `;
-            })
-            .join('')}
-          </div>
-
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              recipe.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceURL}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${icons}.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>`;
-    recipeContainer.innerHTML = '';
-    //do this so that it gets rid of the "look for a recipe thing"
-    recipeContainer.insertAdjacentHTML('afterbegin', markupHTML);
+    console.log(model.state);
+    // 2. RENDERING THE RECIPE
+    recipeView.render(model.state.recipe);
   } catch (error) {
     console.error(error);
   }
 }
 
-// console.log('here');
-showRecipe();
-
-//what happens here might be the way the this keyword is distributed, look at the following lines
 window.addEventListener('hashchange', () => {
   console.log('Hash changed:', window.location.hash);
-  showRecipe();
+  controlRecipe();
 });
-//the hash change only works when the hash is changed, never looks at the initial hash, this does so
-window.addEventListener('load', showRecipe());
+
+window.addEventListener('load', controlRecipe());
--- src/js/model.js
+++ src/js/model.js
@@ -8,7 +8,6 @@ export const loadRecipe = async function (id) {
   try {
     const response = await fetch(
       `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-      // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
     );
     const data = await response.json();
     if (!response.ok) {
@@ -17,13 +16,8 @@ export const loadRecipe = async function (id) {
       );
       throw new Error('This ID does not exist!');
     }
-    //we use let here because we want to make a new object completely, restructuring the formatting from the API
     const { recipe } = data.data;
-    //get recipe, from the data object
-    // console.log(recipe);
-
     state.recipe = {
-      //over here what we actually did is assign the state variable which we export
       id: recipe.id,
       ingredients: recipe.ingredients,
       cookingTime: recipe.cooking_time,
