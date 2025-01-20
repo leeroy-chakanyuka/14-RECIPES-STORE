5451c72d7a7e0c2f089b41a3aa000503d33c7696
Leeroy Chakanyuka <chakanyukaleeroy4@gmail.com>
2025-01-20 22:20:26 +0200

    adding a generic class to be the blueprint for the view

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,1 +1,39 @@
-export default class View {}
+export default class View {
+  //we're going to make this a generic view for all the others to inherit
+  #message;
+  #parentElement = document.querySelector('.recipe');
+  addHandlerRender(callback) {
+    window.addEventListener('load', callback);
+    window.addEventListener('hashchange', () => {
+      callback;
+    });
+  }
+  #clear() {
+    this.#parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const html = ` <div class="spinner">
+            <svg>
+              <use href="${icons}.svg#icon-loader"></use>
+            </svg>
+          </div> 
+          `;
+    this.#parentElement.innerHTML = '';
+    this.#parentElement.insertAdjacentHTML('afterbegin', html);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `<div class="message">
+                    <div>
+                      <svg>
+                        <use href="src/img/${icons}.svg#icon-smile"></use>
+                      </svg>
+                    </div>
+                    <p>${message}</p>
+                  </div> 
+        `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
